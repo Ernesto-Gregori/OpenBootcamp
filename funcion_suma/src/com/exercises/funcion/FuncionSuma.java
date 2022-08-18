e507184c039776db2565bfca3fe6a032463438f7
Oscar Ernesto Gregori Gallardo <46912758+Oscar951@users.noreply.github.com>
2022-08-18 07:42:57 -0600

    Creacion de funciones suma

---
+++ funcion_suma/src/com/exercises/funcion/FuncionSuma.java
@@ -0,0 +1,13 @@
+package com.exercises.funcion;
+
+public class FuncionSuma {
+
+
+
+    static void suma(int number1, int number2, int number3) {
+        int resultado;
+        resultado = (number1 + number2 + number3);
+        System.out.println(resultado);
+    }
+
+}
